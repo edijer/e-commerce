335d6bc8b4bbdcc7aa46ee631b98366e1e7c1bd6
Jhai Sarmiento <edijer@gmail.com>
2020-07-26 11:52:55 +0800

    Intiial Commit - Navbar is partially implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="navbar">
+      <span className="title">eCommerce Site</span>
+      <ul className="main-nav">
+        <li>
+          <a href="#" className="nav-links">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#" className="nav-links">
+            My Orders
+          </a>
+        </li>
+        <li>
+          <a href="#" className="nav-links">
+            Cart
+          </a>
+        </li>
+      </ul>
+      <span className="navbar-toggle">Expand</span>
+    </nav>
   );
 }
 
