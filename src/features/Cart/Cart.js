357761f1cbe861c92670a66b8b5d9cef321f6b0e
Jhai Sarmiento <edijer@gmail.com>
2020-07-27 09:11:26 +0800

    Updated Details and Cart responsive layout

--- src/features/Cart/Cart.js
+++ src/features/Cart/Cart.js
@@ -1,11 +1,70 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 
-const Cart = () => {
+import { setTitle } from "../../stores/currentPageSlice";
+import css from "./Cart.module.css";
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setTitle: (title) => {
+      dispatch(setTitle(title));
+    },
+  };
+};
+
+const mapStateToProps = (state) => {
+  return {
+    cart: state.cart,
+  };
+};
+
+const Cart = (props) => {
+  const { cart, setTitle } = props;
+
+  useEffect(() => {
+    setTitle("Cart");
+
+    return function cleanup() {
+      setTitle("");
+    };
+  }, [setTitle]);
+
   return (
     <div>
-      <h1>Cart</h1>
+      <div className={css.root}>
+        <div className={css.main}>
+          <div className={css.billing}>
+            <div>Shipping Address</div>
+          </div>
+          <div>
+            <div>Shopping Bag</div>
+            <div>Price</div>
+            <hr />
+            <div>
+              {cart.items.map((book) => {
+                return (
+                  <div className={css.cartItemRow} key={book.id}>
+                    <div className={css.cartItemText}>
+                      <div className={css.cartItemTitle}>{book.title}</div>
+                      <div
+                        className={css.cartItemSubTitle}
+                      >{`Author: ${book.author}`}</div>
+                      <div
+                        className={css.cartItemSubTitle}
+                      >{`ISBN: ${book.isbn}`}</div>
+                    </div>
+                    <div className={css.cartItemPrice}>
+                      <div className="text-primary">{book.price}</div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Cart;
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
